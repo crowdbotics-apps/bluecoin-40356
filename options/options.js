f0f047f110e7d3cd0ac83bc8eb3a3118f2835434
Crowdbotics <team@crowdbotics.com>
2023-04-30 09:18:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bluecoin-40356",
+  url: "https://bluecoin-40356.botics.co",
+  api: "https://bluecoin-40356.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
